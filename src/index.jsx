01aa7d4f1bc8a8e2f73b78dba3bae827ac4e76b5
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2019-03-07 02:24:07 +0200

    Boilerplate

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+import store from './store/store';
+import App from './components/App/AppContainer';
+
+import './index.scss';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={createHistory()}>
+      <App />
+    </Router>
+  </Provider>,
+
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const srcPath = path.resolve(__dirname, 'src/');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(srcPath, 'index.jsx'),
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              ["@babel/plugin-proposal-decorators", { "legacy": true }],
+              ["@babel/plugin-proposal-class-properties", { "loose": true }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png|svg)$/,
+        loader: 'url-loader',
+        options: {
+          fallback: 'file-loader',
+          name: '[name].[ext]',
+          outputPath: 'static/img/',
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(srcPath, 'index.html')
+    }),
+    new Dotenv()
+  ],
+  devServer: {
+    hot: true,
+    historyApiFallback: true
+  }
+};
