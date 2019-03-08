9639b0c6620c974fea5535ee3f1ea1f1a9d95d12
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2019-03-08 19:27:13 +0200

    Responsive

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import './TopBar.scss';
-import { timingSafeEqual } from 'crypto';
 
 export default class TopBar extends Component {
   constructor(props) {
