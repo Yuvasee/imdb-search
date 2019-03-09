d25da78ade95d806de2ba40b229a0c0b044d5f89
Yuriy Vasilchikov <yuriy.vasilchikov@gmail.com>
2019-03-09 16:47:38 +0200

    Fixed spinner in modal

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -6,12 +6,17 @@ class Spinner extends React.Component {
   render() {
     const size = this.props.size || '40px';
     const text = this.props.text || '';
+    const color = this.props.color || '#111';
 
     return (
       <div className="spinner-wrapper">
         <div className="spinner" style={{width: size, height: size}}>
-          <div className="a1"></div>
-          <div className="a2"></div>
+          <div className="a1" style={{
+            backgroundColor: color
+          }}></div>
+          <div className="a2" style={{
+            backgroundColor: color
+          }}></div>
         </div>
         <div className="spinner-text">{text}</div>
       </div>
